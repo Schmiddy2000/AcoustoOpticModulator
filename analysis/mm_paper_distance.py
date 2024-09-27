ee2993d3af954d16423c0df1bf88d3e3d7db8141
Schmiddy2000 <luca.schmitt@gmx.net>
2024-09-27 21:47:05 +0200

    Cleaned up bragg angle calculation and added idea for mm paper tilt correction

---
+++ analysis/mm_paper_distance.py
@@ -0,0 +1,16 @@
+# Imports
+
+import numpy as np
+
+
+"""
+Idea:
+Take the image with the largest frequency and most (prominent) dots created by higher order diffraction. 
+Alternatively even use multiple images. 
+
+Here we want to use the center of the 0th order dot as the center of the coordinate system. 
+The other centers should then be assessed relative to this point. 
+We can then do a linear regression, or rather an ODR, since we expect to have fairly large errors in x and y direction. 
+We can then use the parameters obtained to write a function that can turn the measurements measured with neglection 
+of the tilt into corrected values with corresponding uncertainties.  
+"""
